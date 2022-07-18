c4e27a1dbad261cb3d2c03d9a8b5a4d6d940b5ce
Eric Ampire <ericampire.top@gmail.com>
2022-07-18 17:12:12 +0200

    Init commit

---
+++ app/src/main/java/org/esisalama/mobile/project/mobile/HomePageActivity.java
@@ -0,0 +1,15 @@
+package org.esisalama.mobile.project.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
---
+++ app/src/main/java/org/esisalama/mobile/project/mobile/LoginActivity.java
@@ -0,0 +1,58 @@
+package org.esisalama.mobile.project.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText edMatricule = findViewById(R.id.editTextMatricule);
+        EditText edPassword = findViewById(R.id.edittext_password);
+
+        Button boutonLogin = findViewById(R.id.button_login);
+        progressBar = findViewById(R.id.progress_bar);
+
+        // 16AB005
+        boutonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String matricule = edMatricule.getText().toString();
+                String password = edPassword.getText().toString();
+
+                if (matricule.isEmpty()) {
+                    Toast.makeText(
+                            LoginActivity.this,
+                            "Le matricule ne doit pas etre vide",
+                            Toast.LENGTH_LONG
+                    ).show();
+                } else if (password.isEmpty()) {
+                    Toast.makeText(
+                            LoginActivity.this,
+                            "Le mot de passe ne doit pas etre vide",
+                            Toast.LENGTH_LONG
+                    ).show();
+                } else {
+                    login(matricule, password);
+                }
+            }
+        });
+    }
+
+    private void login(String matricule, String password) {
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.GONE);
+    }
+}
